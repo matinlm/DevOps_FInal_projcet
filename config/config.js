6ae4ae6ab20c41b52c6d18f30eb27d540d1daa10
Matin <matin_l_m2003@yahoo.com>
2025-07-14 21:23:55 +0200

    run in minikube

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     server: {
         port: process.env.PORT || 3000,
-        hostname: process.env.HOSTNAME || 'localhost',
+        hostname: process.env.HOSTNAME || '0.0.0.0',
     },
     database: {
-      url: 'mongodb://admin:password@mongo-service:27017',
+      url: 'mongodb://admin:password@mongodb-service:27017',
       name: 'icecream'
     }
   };
